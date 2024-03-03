8d486a4eb494a8887dddf4b0dae71c96696a933c
Penguin Master <PenguinMaster1624@gmail.com>
2024-03-02 21:53:18 -0500

    Refactored many segments, renamed many variables for easier readiability

--- cogs/Hypixel Skyblock/SkyblockStuff.py
+++ cogs/Hypixel Skyblock/SkyblockStuff.py
@@ -13,35 +13,34 @@ class SkyblockItems(commands.Cog):
       async with session.get('https://api.hypixel.net/resources/skyblock/election') as response:
         r = await response.json()
     
-    if response.status == 200:
-      mayor = r['mayor']
-      name = mayor['name']
-      mayor_type = mayor['key']
-      mayor_stuff = mayor['perks']
+    if response.status != 200:
+      await interaction.response.send_message('Data is inacessible')
+      return
     
-      # reformats the mayor type to have the first letter capitalized
-      mayor_type = mayor_type.title()
+    mayor = r['mayor']
+    name = mayor['name']
+    mayor_type: str = mayor['key']
+    mayor_stuff = mayor['perks']
+    
+    # reformats the mayor type to have the first letter capitalized
+    mayor_type = mayor_type.title()
+    perks = {}
 
-      perk_names = []
-      perk_descriptions = []
+    # adds the current mayor's perk information to the corresponding sets
+    for i in range(len(mayor_stuff)):
+      filtered_desc = re.sub(r'§.', '', mayor_stuff[i]['description'])
+      perks[mayor_stuff[i]['name']] = filtered_desc
 
-      # adds the current mayor's perk information to the corresponding sets
-      for i in range(len(mayor_stuff)):
-        perk_names.append(mayor_stuff[i]['name'])
-        perk_descriptions.append(mayor_stuff[i]['description'])
+    # embed creation and setup
+    embed = discord.Embed(title = 'Hypixel Skyblock\'s Current Mayor', color = discord.Color.random())
+    embed.add_field(name = name, value = f'Specialization: {mayor_type}')
 
-      perk_buffs = [re.sub(r'§.', '', perk_descriptions[i]) for i in range(len(mayor_stuff))]
-
-      # embed creation and setup
-      embed = discord.Embed(title = 'Hypixel Skyblock\'s Current Mayor', color = discord.Color.random())
-      embed.add_field(name = name, value = f'Specialization: {mayor_type}')
-
-      # creates individual fields for the mayor's perks
-      for i in range(len(perk_names)):
-        embed.add_field(name = perk_names[i], value = perk_buffs[i], inline = False)
-
-      embed.set_footer(text = 'The mayor is updated roughly every 5 days, 4 hours amd 48 minutes in real life')
-      await interaction.response.send_message(embed = embed)
+    # creates individual fields for the mayor's perks
+    for perk, description in perks.items():
+      embed.add_field(name = perk, value = description, inline = False)
+    
+    embed.set_footer(text = 'The mayor is updated roughly every 5 days, 4 hours amd 48 minutes in real life')
+    await interaction.response.send_message(embed = embed)
       
   @app_commands.command(name = 'election', description = 'Displays information on the current Hypixel Skyblock election')
   async def election(self, interaction: discord.Interaction):
@@ -61,27 +60,21 @@ class SkyblockItems(commands.Cog):
 
       else:
         # individual lists that grab their respective values for each candidate from the API
-        name = [i['name'] for i in election]
-        slot = [i['perks'] for i in election]
-        votes = [i['votes'] for i in election]
-        mayor_type = [i['key'].title() for i in election]
+        name = [mayor['name'] for mayor in election]
+        slot = [mayor['perks'] for mayor in election]
+        votes = [mayor['votes'] for mayor in election]
+        mayor_type = [mayor['key'].title() for mayor in election]
         last_updated = r['lastUpdated']
 
         # creates lists for respective mayors and their perks, for use in the following for loop
-        perk_names = [[] for i in range(5)]
-        perk_descriptions = [[] for i in range(5)]
+        perks = [{} for i in range(5)]
   
         # adds the candidates' perks to their respective mayors
-        for i in range(len(slot)):
-          for j in slot[i]:
-            perk_names[i].append(j['name'])
-            perk_descriptions[i].append(j['description'])
-
-        # removes the simoleon and the character following it, for better readability
-        for i in range(len(perk_descriptions)):
-          for j in range(len(perk_descriptions[i])):
-            perk_descriptions[i][j] = re.sub(r'§.', '', perk_descriptions[i][j])
-
+        for mayor in range(len(slot)):
+          for perk in slot[mayor]:
+            filtered_desc = re.sub(r'§.', '', perk['description'])
+            perks[mayor][perk['name']] = filtered_desc
+            
         percentages = []
         for i in range(len(votes)):
           percentages.append(round((int(votes[i])/sum(map(int, votes)))*100, 2))
@@ -96,7 +89,7 @@ class SkyblockItems(commands.Cog):
         
         # creates individual fields for each mayor and their perks
         for i in range(5):
-          perk = '\n'.join(f'__{name}__\n*{description}*\n' for (name, description) in zip(perk_names[i], perk_descriptions[i]))
+          perk = '\n'.join(f'__{name}__\n*{description}*\n' for name, description in perks[i].items())
           embed.add_field(name = name[i], value = f'Specialization: {mayor_type[i]}\n\n{perk}\nVotes: {votes[i]} (**{percentages[i]}%**)', inline = False)
         
         embed.set_footer(text = f'Total amount of votes: {all_votes}')
@@ -108,61 +101,50 @@ class SkyblockItems(commands.Cog):
 
     if r.status_code == 200:
       data = r.json()['products']
-      
-      lst = []
-      item = ''
-
-      for letter in item_name:
-        lst.append(letter)
-
-        # changes any lowercase letters to uppercase and adds an underscore in place of spaces
-        # this is done so in order to match the passed item to an id
-        for lower in range(len(lst)):
-          lst[lower] = lst[lower].upper()
-          lst[lower] = re.sub(' ', '_', lst[lower])
-        item += lst[lower]
+      item = item_name.upper().strip().replace(' ', '_')
 
       # if statement checks to see if the passed item -- processed -- exists in the bazaar
-      if item in data:
-        # gets all the prices
-        item_info = data[item]
-        item_quick = item_info['quick_status']
-        instant_buy_price = item_quick['buyPrice']
-        instant_sell_price = item_quick['sellPrice']
-        
-        buy_offer_price = item_info['buy_summary'][0]['pricePerUnit']
-        sell_offer_price = item_info['sell_summary'][0]['pricePerUnit']
-
-        # gets the amount being offered or ordered for the top offer/order
-        # also how many orders are at the top
-        buy_offer_amount = item_info['buy_summary'][0]['amount']
-        sell_offer_amount = item_info['sell_summary'][0]['amount']
-
-        # combined all of the prices and runs it through a for loop for formating
-        prices = [instant_buy_price, instant_sell_price, buy_offer_price, sell_offer_price]
-
-        for i in range(len(prices)):
-          prices[i] = round(float(prices[i]), 1)
-          prices[i] = '{:,}'.format(prices[i])
-
-        buy_offer_amount = '{:,}'.format(buy_offer_amount)
-        sell_offer_amount = '{:,}'.format(sell_offer_amount)
-
-        bazaar_info = f'Instant Buy Price: {prices[0]}\nInstant Sell Price: {prices[1]}\n\nTop Buy Order Price: {prices[2]}\nTop Amount Wanted: {buy_offer_amount}\n\nTop Sell Offer Price: {prices[3]}\nTop Amount Offered: {sell_offer_amount}'
-        
-        # setup of embed
-        embed = discord.Embed(title = 'Hypixel Skyblock Bazaar', color = discord.Color.random())
-        embed.add_field(name = item, value = bazaar_info)
-        
-        await interaction.response.send_message(embed = embed)
-
-      # if the passed item doesn't exist in the bazaar, this is sent
-      # this is done in case of a typo
-      else:
+      if item not in data:
         await interaction.response.send_message('The bazaar doesn\'t sell that. Maybe there was a typo?')
+        return 
+      
+      # gets all the prices
+      item_info = data[item]
+      item_quick = item_info['quick_status']
+      instant_buy_price = item_quick['buyPrice']
+      instant_sell_price = item_quick['sellPrice']
+      
+      buy_offer_price = item_info['buy_summary'][0]['pricePerUnit']
+      sell_offer_price = item_info['sell_summary'][0]['pricePerUnit']
+
+      # gets the amount being offered or ordered for the top offer/order
+      # also how many orders are at the top
+      buy_offer_amount = item_info['buy_summary'][0]['amount']
+      sell_offer_amount = item_info['sell_summary'][0]['amount']
+
+      # combined all of the prices and runs it through a for loop for formating
+      prices = [instant_buy_price, instant_sell_price, buy_offer_price, sell_offer_price]
+
+      for i in range(len(prices)):
+        prices[i] = round(float(prices[i]), 1)
+        prices[i] = '{:,}'.format(prices[i])
+
+      buy_offer_amount = '{:,}'.format(buy_offer_amount)
+      sell_offer_amount = '{:,}'.format(sell_offer_amount)
+
+      bazaar_info = f'Instant Buy Price: {prices[0]}\nInstant Sell Price: {prices[1]}\n\nTop Buy Order Price: {prices[2]}\nTop Amount Wanted: {buy_offer_amount}\n\nTop Sell Offer Price: {prices[3]}\nTop Amount Offered: {sell_offer_amount}'
+      
+      # setup of embed
+      embed = discord.Embed(title = 'Hypixel Skyblock Bazaar', color = discord.Color.random())
+      embed.add_field(name = item, value = bazaar_info)
+      
+      await interaction.response.send_message(embed = embed)
       
     elif r.status_code == 503:
       await interaction.response.send_message('Data hasn\'t loaded yet, try again in a bit')
+    
+    else:
+      await interaction.response.send_message('The data is inaccessible, the Hypixel API might be down')
 
 async def setup(bot: commands.Bot):
   await bot.add_cog(SkyblockItems(bot))
