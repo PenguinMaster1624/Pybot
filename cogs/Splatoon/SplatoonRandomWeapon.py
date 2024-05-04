1ecad7aebad0a9197e5233aaa298e030ba626f2a
Penguin Master <PenguinMaster1624@gmail.com>
2024-05-04 03:23:41 -0400

    Adjusted for database moving to another folder, no longuer using regex for sub/special/weapon class. More info shown in embed when command is used

--- cogs/Splatoon/SplatoonRandomWeapon.py
+++ cogs/Splatoon/SplatoonRandomWeapon.py
@@ -1,36 +1,42 @@
 from discord.ext import commands
 from discord import app_commands
-import random, discord, sqlite3, re
+import random, discord, sqlite3
 
-class Splat2nRandomWeapon(commands.Cog):
+class Splatoon3RandomWeapon(commands.Cog):
   def __init__(self, bot: commands.Bot):
     self.bot = bot
-    self.db_path = 'cogs/Splatoon/SplatoonUtils/Splatoon 3.db'
+    self.db_path = 'Utils/Game Stuff.db'
+
+  async def db_connect(self, command: str, select: tuple[str | None]) -> list[tuple[str]]:
+    with sqlite3.connect(self.db_path) as db:
+      cursor = db.cursor()
+      if all(select):
+        return [weapon for weapon in cursor.execute(command, select)]
+
+      else:
+        return [weapon for weapon in cursor.execute(command)]
+
 
   @app_commands.command(name = 'rsw', description = 'Rolls a random Splatoon 3 weapon')
   async def rsw(self, interaction: discord.Interaction, weapon_class: str = None):
     if weapon_class is not None:
       weapon_class = weapon_class.title().strip()
-      search = re.search('^Shooter$|^Roller$|^Charger$|^Slosher$|^Splatling$|^Dualies$|^Brella$|^Blaster$|^Brush$|^Stringer$|^Splatana$', weapon_class)
-      if search:
-        command = 'SELECT Main, Introduced FROM Weapons WHERE Class = ?'
+      if weapon_class in ['Shooter', 'Roller', 'Charger', 'Slosher', 'Splatling', 'Dualies', 'Brella', 'Blaster', 'Brush', 'Stringer', 'Splatana']:
+        command = 'SELECT Main, Class, Introduced FROM "Splatoon 3 Weapons" WHERE Class = ?'
       
       else:
         await interaction.response.send_message(f'{weapon_class} is not a valid weapon class', ephemeral = True)
     
     else:
-      command = 'SELECT Main, Introduced FROM Weapons'
+      command = 'SELECT Main, Class, Introduced FROM "Splatoon 3 Weapons"'
 
-    with sqlite3.connect(self.db_path) as db:
-      cursor = db.cursor()
-      weapons = [weapon for weapon in cursor.execute(command, (weapon_class,))]
-
+    weapons = await self.db_connect(command = command, select = (weapon_class,))
     selection = random.choice(weapons)
 
     embed = discord.Embed(title = 'Splatoon 3 Random Weapon Selector', color = discord.Color.blue())
     embed.set_author(name = interaction.user.name, icon_url = interaction.user.avatar)
     embed.add_field(name = 'The Council Has Decided Your Fate!', value = selection[0])
-    embed.set_footer(text = f'Introduced In {selection[1]}')
+    embed.set_footer(text = f'Introduced In {selection[2]}')
     
     await interaction.response.send_message(embed = embed, ephemeral = True)
 
@@ -42,14 +48,11 @@ class Splat2nRandomWeapon(commands.Cog):
   @app_commands.command(name = 'rss', description = 'Rolls a random Splatoon 3 weapon based on Sub Weapon')
   async def rss(self, interaction: discord.Interaction, sub_weapon: str):
     sub_weapon = sub_weapon.title().strip()
-    search = re.search('^Splat Bomb$|^Suction Bomb$|^Burst Bomb$|^Splash Wall$|^Fizzy Bomb$|^Curling Bomb$|^Auto Bomb$|^Squid Beakon$|^Point Sensor$|^Ink Mine$|^Toxic Mist$|^Angle Shooter$|^Torpedo$', sub_weapon)
-    if not search:
+    if not sub_weapon in ['Splat Bomb', 'Suction Bomb', 'Burst Bomb', 'Sprinkler', 'Splash Wall', 'Fizzy Bomb', 'Curling Bomb', 'Auto Bomb', 'Squid Beakon', 'Point Sensor', 'Ink Mine', 'Toxic Mist', 'Angle Shooter', 'Torpedo']:
       await interaction.response.send_message('That is not a valid sub weapon', ephemeral = True)
       return
       
-    with sqlite3.connect(self.db_path) as db:
-      cursor = db.cursor()
-      weapons = [weapon for weapon in cursor.execute('SELECT Main, Introduced FROM Weapons WHERE Sub = ?', (sub_weapon,))]
+    weapons = await self.db_connect(command = 'SELECT Main, Introduced FROM "Splatoon 3 Weapons" WHERE Sub = ?', select = (sub_weapon,))
 
     selection = random.choice(weapons)
 
@@ -68,16 +71,12 @@ class Splat2nRandomWeapon(commands.Cog):
   @app_commands.command(name = 'rsp', description = 'Rolls a random Splatoon 3 weapon based on Special')
   async def rsp(self, interaction: discord.Interaction, special: str):
     special = special.title().strip()
-    search = re.search('^Trizooka$|^Big Bubbler$|^Zipcaster$|^Tenta Missiles$|^Ink Storm$|^Booyah Bomb$|^Wave Breaker$|^Ink Vac$|^Killer Wail 5.1$|^Inkjet$|^Ultra Stamp$|^Crab Tank$|^Triple Inkstrike$|^Tacticooler$|^Super Chump$|^Kraken Royale$', special)
-    if not search:
+    if not special in ['Trizooka', 'Big Bubbler', 'Zipcaster', 'Tenta Missiles', 'Ink Storm', 'Booyah Bomb', 'Wave Breaker', 'Ink Vac', 'Killer Wail 5.1', 'Inkjet', 'Ultra Stamp', 'Crab Tank', 'Triple Inkstrike', 'Tacticooler', 'Super Chump', 'Kraken Royale']:
       await interaction.response.send_message('That is not a valid special', ephemeral = True)
       return
     
-    with sqlite3.connect(self.db_path) as db:
-      cursor = db.cursor()
-      weapons = [weapon for weapon in cursor.execute('SELECT Main, Introduced FROM Weapons WHERE Special = ?', (special,))]
-      selection = random.choice(weapons)
-
+    weapons = await self.db_connect(command = 'SELECT Main, Introduced FROM "Splatoon 3 Weapons" WHERE Special = ?', select = (special,))
+    selection = random.choice(weapons)
 
     embed = discord.Embed(title = 'Splatoon 3 Weapon Randomizer Based On Special', description = f'{special} go brrr', color = discord.Color.purple())
     embed.set_author(name = interaction.user.name, icon_url = interaction.user.avatar)
@@ -93,4 +92,4 @@ class Splat2nRandomWeapon(commands.Cog):
   
 
 async def setup(bot: commands.Bot):
-  await bot.add_cog(Splat2nRandomWeapon(bot), guild = None)
+  await bot.add_cog(Splatoon3RandomWeapon(bot), guild = None)
