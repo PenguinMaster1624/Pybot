b9d38762ff2b8a1cab147f566180f008c2468e78
Penguin Master <PenguinMaster1624@gmail.com>
2026-06-13 13:54:33 -0400

    Splatfest Team Picker updated, under watch pending Splatfest

--- cogs/Splatoon/SplatfestTeamSelector.py
+++ cogs/Splatoon/SplatfestTeamSelector.py
@@ -1,51 +1,38 @@
-from SplatoonUtils.SplatoonUtils import generate_timestamp
-from discord import app_commands
+from models import FestivalsResponse, Votes
+from discord import app_commands, User
+from utils.sessions import fetch_data
 from discord.ext import commands
-import discord, requests
-
-
-def fetch_data() -> dict:
-    with requests.get('https://splatoon3.ink/data/festivals.json') as response:
-        js = response.json()
-        return js['US']['data']['festRecords']['nodes'][0]
-        
-def team_names() -> list[str]:
-    data = fetch_data()
-    return [data['teams'][i]['teamName'] for i in range(3)]
-
-teams = team_names()
+import discord
 
 class SplatfestButtons(discord.ui.View):
-    def __init__(self, embed: discord.Embed) -> None:
+    def __init__(self, embed: discord.Embed, bot: commands.Bot) -> None:
         super().__init__(timeout = None)
+        self.bot = bot
         self.embed = embed
 
-        self.total = {
-            teams[0]: [],
-            teams[1]: [],
-            teams[2]: []
-        }
 
-    async def player_check(self, user_name: str, team: str) -> str | None:
-        for person, players in self.total.items():
-            if person != team:
-                if user_name in players:
-                    players.remove(user_name)
-            
-            elif person == team and user_name in players:
+    async def setup(self) -> None:
+        self.model = await fetch_data(url='https://splatoon3.ink/data/festivals.json', model=FestivalsResponse)
+        self.votes = [Votes(team_name=team.name) for team in self.model.teams]
+
+    async def player_check(self, member: User, team: str) -> str | None:
+        for teams in self.votes:
+            if team == teams.team_name and member in teams.members:
                 return 'You\'re already on that team, you can\'t vote for the same team you\'re repping!'
-                
+            
+            elif team != teams.team_name and member in teams.members:
+                teams.members.remove(member)
+            
             else:
-                players.append(user_name)
+                teams.members.append(member)
 
     async def embed_setup(self) -> discord.Embed:
-        players = list(self.total.values())
         for num in range(len(self.children)):
-            self.children[num].label = f'{teams[num]}: {len(players[num])}'
+            self.children[num].label = f'{self.votes[num].team_name}: {len(self.votes[num].members)}'
 
-        for pos in range(len(players)):
-            player_list = '\n'.join(players[pos])
-            self.embed.set_field_at(index=pos, name = teams[pos], value = player_list if player_list else None)
+        for index in range(len(self.votes)):
+            player_list = f'\n'.join(self.votes[index].members)
+            self.embed.set_field_at(index=index, name=self.votes[index].team_name, value= player_list if player_list else None)
 
     async def embed_change(self, interaction: discord.Interaction, helper: str | None) -> None:
         if interaction.response.is_done() is False and helper is not None:
@@ -55,24 +42,21 @@ class SplatfestButtons(discord.ui.View):
             await self.embed_setup()
             await interaction.response.edit_message(embed = self.embed, view = self)
 
-    @discord.ui.button(label = teams[0], style = discord.ButtonStyle.blurple)
+    @discord.ui.button(label = 'Shiver', style = discord.ButtonStyle.blurple)
     async def Shiver(self, interaction: discord.Interaction, button: discord.ui.Button):
-        user_name = interaction.user.display_name
-        helper = await self.player_check(user_name = user_name, team = teams[0])
+        helper = await self.player_check(member=interaction.user, team = self.model.teams[0].name)
 
         await self.embed_change(interaction=interaction, helper=helper)
 
-    @discord.ui.button(label = teams[1], style = discord.ButtonStyle.red)
+    @discord.ui.button(label = 'Frye', style = discord.ButtonStyle.red)
     async def Frye(self, interaction: discord.Interaction, button: discord.ui.Button):
-        user_name = interaction.user.display_name
-        helper = await self.player_check(user_name = user_name, team = teams[1])
+        helper = await self.player_check(member=interaction.user, team=self.model.teams[1].name)
 
         await self.embed_change(interaction=interaction, helper=helper)
 
-    @discord.ui.button(label = teams[2], style = discord.ButtonStyle.green)
+    @discord.ui.button(label = 'Big Man', style = discord.ButtonStyle.green)
     async def Beeg_Man(self, interaction: discord.Interaction, button: discord.ui.Button):
-        user_name = interaction.user.display_name
-        helper = await self.player_check(user_name = user_name, team = teams[2])
+        helper = await self.player_check(member=interaction.user, team = self.model.teams[2].name)
 
         await self.embed_change(interaction=interaction, helper=helper)
 
@@ -84,25 +68,27 @@ class SplatfestTeamChoices(commands.Cog):
     @app_commands.allowed_installs(users=True, guilds=True)
     @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
     async def SplatfestTeams(self, interaction: discord.Interaction):
-        data = fetch_data()
+        model = await fetch_data(url='https://splatoon3.ink/data/festivals.json', model=FestivalsResponse)
+        print(model)
         if await self.bot.is_owner(interaction.user) is False:
             await interaction.response.send_message(content = 'You need to be the bot owner to use this command', ephemeral = True)
             return
         
-        elif data['isVotable'] is False:
+        elif model.is_votable is False:
             await interaction.response.send_message('No Splatfest soon', ephemeral = True)
             return
     
-        start_time = await generate_timestamp(time=data['startTime'])
-        end_time = await generate_timestamp(time=data['endTime'])
-        embed = discord.Embed(title = data['title'], description = f"Start Time: <t:{start_time}:t>,<t:{start_time}:R>\nEnd TIme: <t:{end_time}:t>, <t:{end_time}:R>", color=discord.Color.orange())
+        start = int(model.start)
+        end = int(model.end)
+
+        embed = discord.Embed(title = model.title, description = f"Start Time: <t:{start}:t>,<t:{start}:R>\nEnd TIme: <t:{end}:t>, <t:{end}:R>", color=discord.Color.orange())
         for i in range(3):
-            embed.add_field(name = data['teams'][i]['teamName'], value = data['teams'][i]['teamName'], inline = True)
+            embed.add_field(name = model.teams[i].name, value = model.teams[i].name, inline = True)
         
-        embed.set_image(url = data['image']['url'])
+        embed.set_image(url = model.image_url)
         embed.set_footer(text = 'Happy Splatting :)')
 
-        await interaction.response.send_message(embed = embed, view = SplatfestButtons(embed=embed))
+        await interaction.response.send_message(embed = embed, view = SplatfestButtons(embed=embed, bot=self.bot))
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(SplatfestTeamChoices(bot))
