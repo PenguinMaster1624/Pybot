262a860a79e8ab6f60fdd80a84ad79d067bec0e0
Penguin Master <PenguinMaster1624@gmail.com>
2026-06-22 16:13:52 -0400

    Stripped empty lines of whitespace

--- orm_models/S3KitModels.py
+++ orm_models/S3KitModels.py
@@ -1,5 +1,5 @@
-from sqlalchemy.orm import Mapped, mapped_column, relationship, DeclarativeBase
-from sqlalchemy import String, ForeignKey, BLOB
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import String, ForeignKey, BLOB, Integer
 
 
 class Base(DeclarativeBase):
@@ -8,14 +8,14 @@ class Base(DeclarativeBase):
 class S3Kits(Base):
   __tablename__ = 's3_kits'
 
-  id: Mapped[int] = mapped_column(primary_key=True)
-  main: Mapped[str] = mapped_column(String(19))
-  sub_id: Mapped[int] = mapped_column(ForeignKey('s3_subs.id'))
-  special_id: Mapped[int] = mapped_column(ForeignKey('s3_specials.id'))
-  class_id: Mapped[int] = mapped_column(ForeignKey('s3_classes.id'))
-  points_for_special: Mapped[int] = mapped_column()
-  introduced: Mapped[str] = mapped_column(String(35))
-  image: Mapped[bytes] = mapped_column(BLOB)
+  id: Mapped[int] = mapped_column(Integer, primary_key=True)
+  main: Mapped[str] = mapped_column(String(19), nullable=False)
+  sub_id: Mapped[int] = mapped_column(ForeignKey('s3_subs.id'), nullable=False)
+  special_id: Mapped[int] = mapped_column(ForeignKey('s3_specials.id'), nullable=False)
+  class_id: Mapped[int] = mapped_column(ForeignKey('s3_classes.id'), nullable=False)
+  points_for_special: Mapped[int] = mapped_column(Integer, nullable=False)
+  introduced: Mapped[str] = mapped_column(String(35), nullable=False)
+  image: Mapped[bytes] = mapped_column(BLOB, nullable=False)
 
   sub: Mapped["S3Subs"] = relationship(back_populates='kit')
   special: Mapped["S3Specials"] = relationship(back_populates='kit')
@@ -24,19 +24,19 @@ class S3Kits(Base):
 class S3Subs(Base):
   __tablename__ = 's3_subs'
 
-  id: Mapped[int] = mapped_column(primary_key=True)
-  name: Mapped[str] = mapped_column(String(10))
-  ink_consumption: Mapped[int] = mapped_column()
-  image: Mapped[bytes] = mapped_column(BLOB)
+  id: Mapped[int] = mapped_column(Integer, primary_key=True)
+  name: Mapped[str] = mapped_column(String(10), nullable=False)
+  ink_consumption: Mapped[int] = mapped_column(Integer, nullable=False)
+  image: Mapped[bytes] = mapped_column(BLOB, nullable=False)
 
   kit: Mapped[list["S3Kits"]] = relationship(back_populates='sub')
 
 class S3Specials(Base):
   __tablename__ = 's3_specials'
 
-  id: Mapped[int] = mapped_column(primary_key=True)
-  name: Mapped[str] = mapped_column(String(9))
-  image: Mapped[bytes] = mapped_column(BLOB)
+  id: Mapped[int] = mapped_column(Integer, primary_key=True)
+  name: Mapped[str] = mapped_column(String(9), nullable=False)
+  image: Mapped[bytes] = mapped_column(BLOB, nullable=False)
 
   kit: Mapped[list["S3Kits"]] = relationship(back_populates='special')
 
@@ -44,7 +44,7 @@ class S3Classes(Base):
   __tablename__ = 's3_classes'
 
   id: Mapped[int] = mapped_column(primary_key=True)
-  name: Mapped[str] = mapped_column(String(8))
-  image: Mapped[bytes] = mapped_column(BLOB)
+  name: Mapped[str] = mapped_column(String(8), nullable=False)
+  image: Mapped[bytes] = mapped_column(BLOB, nullable=False)
 
   kit: Mapped[list['S3Kits']] = relationship(back_populates='s3_class')
